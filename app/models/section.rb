e662a5f6c034a6378309aa566dc2168b9e98975d
Shashank Bhushan <skbhu@iitk.ac.in>
2016-08-06 14:35:55 +0530

    after 6/7/2016

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,8 @@ class Section < ApplicationRecord
 
 	has_many :section_edits
 	has_many :editors, :through => :section_edits, :class_name => "AdminUser"
-end
+    scope :visible, lambda { where(:visible => true)}
+	scope :invisible, lambda { where(:visible => false)}
+	scope :sorted, lambda { order("sections.position ASC")}
+	scope :newest_first, lambda { order("section.creadted_at DESC")}
+	end
